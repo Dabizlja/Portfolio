16f8b0a1d15bf471ccf2e09c26a62057a87fb5be
Dabizlja <dabizlja@gmail.com>
2017-10-12 12:38:04 +0200

    Integreted new and create for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,4 +4,32 @@ class ProjectsController < ApplicationController
     @projects = Project.all
   end
 
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    respond_to do |format|
+      if @project.save
+        format.html { redirect_to projects_path, notice: "Succesfuly created" }
+      else
+        format.html { render :new }
+      end
+    end
+    
+  end
+
+
+  private
+
+  def set_project
+    @project = Project.find(params[:id])
+  end
+
+  def project_params
+    params.require(:project).permit(:title, :subtitle, :body, :main_image, :thumb_image)
+  end
+
+
 end
